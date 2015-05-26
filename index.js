4c007fc5fb0bb91cbc6e1bb97850d78709b40e0d
tundrax <sherzod@gmail.com>
2015-05-26 14:19:29 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var co = require('co');
+
+function *noop() {}
+
+function compose(filters){
+  return function *(next){
+    if (!next) next = noop();
+
+    var i = filters.length;
+
+    while (i--) {
+      next = filters[i].call(this, next);
+    }
+
+    yield *next;
+  }
+}
+
+// you can create any group of filters,
+// not just limit to rethinkdb api
+var filters = [
+
+  function *(next) {
+    console.log(this); // see this is 'article' passed below
+    console.log('pre-1');
+    yield next;
+    console.log('post-1');
+  },
+
+  function *(next) {
+    console.log('pre-2');
+    yield next;
+    console.log('post-2');
+  },
+
+  function *(next) {
+    console.log('pre-3');
+    yield next;
+    console.log('post-3');
+  }
+];
+
+// assume this is your 'article'
+var article = {
+  title: 'filters with genertors'
+}
+
+var it = co.wrap(compose(filters));
+it.call(article);
